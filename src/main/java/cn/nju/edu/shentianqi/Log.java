2ff6f1633ef4ae7dee7ceadc9cfe8360dc32fddb
shentianqi <1055862487@qq.com>
2021-10-28 20:20:32 +0800

    Log class ver1 现在的配置在log类内部 没有专门的配置类，因为全程序预计配置很少

---
+++ src/main/java/cn/nju/edu/shentianqi/Log.java
@@ -0,0 +1,40 @@
+package cn.nju.edu.shentianqi;
+
+/**
+ * 基本上 这个类就是为了统一管理输出 除这里外，程序其他地方将不允许有stdout stderr
+ */
+public class Log {
+    public static boolean verbose = false;
+
+    public static void printRes(double res) {
+        //仅用于输出结果
+        System.out.printf("%.2f%n", res);
+    }
+
+    public static void out(Object x) {
+        if (verbose)
+            System.out.println(x);
+    }
+
+    public static void err(Object x) {
+        System.err.println(x);
+    }
+
+    public static void main(String[] args) {
+        printRes(0.11111111);
+        printRes(0.15555);
+        printRes(0.000000);
+        printRes(0.505000000);
+        printRes(0.544);
+        verbose = true;
+        out(233);
+        out("dgsgfksj");
+        out(12 + "  ");
+        err("err: 21412");
+        verbose = false;
+        out(233);
+        out("dgsgfksj");
+        out(12 + "  ");
+        err("err: 21412");
+    }
+}
